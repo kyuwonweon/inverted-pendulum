f2825d4a58c170e2bd43d9d2cfee11a12144c81d
kyuwon <kyuwon0917@gmail.com>
2026-08-20 11:50:20 -0500

    pen sim sub/pub/timer/parameters added

--- pendulum_sim/src/pen_sim.cpp
+++ pendulum_sim/src/pen_sim.cpp
@@ -1,4 +1,20 @@
 #include <rclcpp/rclcpp.hpp>
+#include "pendulum_interfaces/msg/pendulum_state.hpp"
+#include "pendulum_interfaces/msg/pendulum_command.hpp"
+
+using namespace std::chrono_literals;
+
+constexpr double M  = 1.0;  // cart (kg)
+constexpr double m = 0.1;  // pole (kg)
+constexpr double l = 0.5;  // pole com (m)
+constexpr double g = 9.81;  // gravity
+
+struct State {
+  double x;
+  double x_dot;
+  double theta;
+  double theta_dot;
+};
 
 class PendulumSim : public rclcpp::Node
 {
@@ -7,6 +23,27 @@ class PendulumSim : public rclcpp::Node
     : Node("pendulum_sim")
     {
       RCLCPP_INFO(this->get_logger(), "pendulum sim started");
+      state_pub_ = this-> create_publisher<pendulum_interfaces::msg::PendulumState>("pendulum_state", 10);
+      command_sub_ = this-> create_subscription<pendulum_interfaces::msg::PendulumCommand>(
+        "pendulum_command", 10, std::bind(&PendulumSim::command_cb, this, std::placeholders::_1)
+      );
+      timer_ = this->create_wall_timer(1ms, std::bind(&PendulumSim::timer_cb, this));
+    }
+
+  private:
+    State state_ = {0.0, 0.0, 0.15, 0.0};
+
+    rclcpp::Publisher<pendulum_interfaces::msg::PendulumState>::SharedPtr state_pub_;
+    rclcpp::Subscription<pendulum_interfaces::msg::PendulumCommand>::SharedPtr command_sub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    void command_cb(const pendulum_interfaces::msg::PendulumCommand::SharedPtr msg)
+    {
+      RCLCPP_INFO(this->get_logger(), "recieved force: %f", msg->cart_force);
+    }
+
+    void timer_cb(){
+      RCLCPP_INFO(this->get_logger(), "add details later");
     }
 };
 
